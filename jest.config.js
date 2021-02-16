f536a7b776154825f4d5cb1b6a2d07e5fc971230
Iurii Tatishchev <itatishch@gmail.com>
2021-02-15 17:24:50 -0800

    fix deps for lint and test

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
+const esModules = [
+  'vee-validate/dist/rule',
+  // 'vuex-composition-helpers/dist'
+  'vuex-composition-helpers/src',
+  '@babel'
+].join('|');
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`]
 };
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,9 +1,14 @@
 import { shallowMount } from '@vue/test-utils';
+import Vuetify from 'vuetify';
+
 import Demo from '@/Module/Module.vue';
 
+const vuetify = new Vuetify();
 describe('Module.vue', () => {
   it("Let's organizers give instructions for students to submit demo videos with adjustable maximum length", () => {
-    const wrapper = shallowMount(Demo, {});
-    expect(wrapper.text()).toMatch('Demonstration');
+    const wrapper = shallowMount(Demo, {
+      vuetify
+    });
+    expect(wrapper.html()).toContain('module__header');
   });
 });
