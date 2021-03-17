f1008e32cfe1124ae5e3e77820d90ba8d8698163
Derick Lee <dericklee@pilotcity.com>
2021-03-17 00:13:56 -0700

    version update

--- src/Module/components/ModuleDefault.vue
+++ src/Module/components/ModuleDefault.vue
@@ -36,14 +36,7 @@
           </v-expansion-panel>
         </v-expansion-panels>
       </div>
-      <v-progress-linear
-        class="module-default__collapse-divider"
-        color="#dedede"
-        height="2"
-        value="100"
-        buffer-value="100"
-        stream
-      />
+    <v-progress-linear class="mt-3" color="#dedede" height="2" value="100" buffer-value="100" stream />
         <div class="module-default__row mt-12">
           <validation-provider
             v-slot="{ errors }"
